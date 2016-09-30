90e2aad35a90390544e7ec602dcba062637c635a
hokagenaruto <bloodspain@gmail.com>
2016-09-30 15:18:24 +0530

    js to find the coordiantes of the address given by the user

--- Source/source_code/ssad/settings.py
+++ Source/source_code/ssad/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'location_based_advertisement',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'the c-13',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
