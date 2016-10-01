4f5080f5daf1265c2e118c0291172b0278a9b05e
hokagenaruto <bloodspain@gmail.com>
2016-10-01 15:39:34 +0530

    changes to global values and intilisation script

--- Source/source_code/global_values.py
+++ Source/source_code/global_values.py
@@ -1,3 +1,5 @@
+import math  #for floor funciton
+
 #limiting longitutde and latitde around THE INDIA
 # left_extreme = 68.03215
 # right_extreme = 97.16712
@@ -8,3 +10,10 @@ left_extreme = 78.2311237
 right_extreme = 78.692392
 top_extreme = 17.599900
 bottom_extreme = 17.2015163
+
+#SIZE OF EACH SQUARE
+delx = 5.0/111 # width of zone = 5Km
+dely = 5.0/111 # height of zone = 5Km
+
+zonesAlongX =  math.floor((right_extreme - left_extreme )/delx)
+zonesAlongY = math.floor((top_extreme - bottom_extreme)/dely)
