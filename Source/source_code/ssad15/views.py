b8aa437185965c9eb6fcc1d4197cf40ac272ee32
hokagenaruto <bloodspain@gmail.com>
2016-10-01 15:57:20 +0530

    adding getzone functionality

--- Source/source_code/ssad15/views.py
+++ Source/source_code/ssad15/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from global_values import *
 
 def index(request):
     return HttpResponse("Hello")
@@ -8,11 +8,15 @@ def index(request):
 
 # Get zone correponding to the location pinged by the device
 def getzone(longitude,latitude):
-
-
+    x = longitude
+    y = latitude
+    rows_done = math.floor((y- bottom_extreme)/dely)
+    in_a_row = math.floor((x - left_extreme)/delx)
+    zone_no = rows_done*zonesAlongX + in_a_row
+    return int(zone_no)
 # get advertisment corresponding to the zone device is in and also the server time
 def get_advertisement(zone_id):
-    
+
 
 #get the pinged location from the device
 def pinged_location(request):
