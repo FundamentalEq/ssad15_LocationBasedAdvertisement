f5cb04a71f7fa5fe85a569aa38694071f8712355
kritz3112 <kritika.agrawal@research.iiit.ac.in>
2016-10-01 19:08:17 +0530

    Display advertisemnet functionality

--- Source/source_code/ssad15/views.py
+++ Source/source_code/ssad15/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from global_values import *
+from django.template import loader
+from .models import zone,slot,advertisement
+from django.shortcuts import get_list_or_404,get_object_or_404
+import datetime
 
 def index(request):
-    return HttpResponse("Hello")
+    return HttpResponse("Location Based Advertising")
 
 
 # Get zone correponding to the location pinged by the device
@@ -14,12 +18,25 @@ def getzone(longitude,latitude):
     in_a_row = math.floor((x - left_extreme)/delx)
     zone_no = rows_done*zonesAlongX + in_a_row
     return int(zone_no)
+
 # get advertisment corresponding to the zone device is in and also the server time
 def get_advertisement(zone_id):
-
+    current_time=datetime.datetime.now()
+    #calculating slot number from 1 to 120
+    slot_no=int((current_time.minute * 60 + current_time.second)/30)
+    slot=get_object_or_404(slot,zone_id=zone_id,slot_no=slot_no)
+    ad_id=slot.advertisement_id
+    ad=get_object_or_404(advertisement,pk=ad_id)
+    path=ad.upload
+    path=str(path)
+    return path
 
 #get the pinged location from the device
-def pinged_location(request):
+#get corresponding zone no and display advertisement according to time and zone
+#this function to be changed for scheduling in R2
+def display_advertisement(request):
+    #checking if location is posted or not
+    #error set to 1 represents an error in getting location of the device
     if request.method == 'POST':
         if 'longitude' in request.POST:
             longitude = request.POST['longitude']
@@ -34,4 +51,6 @@ def pinged_location(request):
         return HttpResponse("Error in getting location !")
     else :
         zone_no=getzone(longitude,latitude)
-        get_advertisement(zone_no)
+        path=get_advertisement(zone_no)
+        context={'path':path}
+        return render(request, 'ssad15/display_advertisement.html', context)
