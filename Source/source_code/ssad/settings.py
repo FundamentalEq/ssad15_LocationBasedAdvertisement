1c62a4584419b9ddf7f061d905d03d4c9b9d734b
kritz3112 <kritika.agrawal@research.iiit.ac.in>
2016-11-01 21:53:18 +0530

    linking no_of_slots js

--- Source/source_code/ssad/settings.py
+++ Source/source_code/ssad/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lba',
         'USER': 'root',
-        'PASSWORD': 'the c-13',
+        'PASSWORD': 'whyso123098',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
