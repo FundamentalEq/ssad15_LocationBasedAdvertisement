6a51aa1df33874671e6c5b1e02f9840092520321
hokagenaruto <bloodspain@gmail.com>
2016-11-12 00:32:24 +0530

    improvements to initialize_zone.py

--- Source/source_code/initialize_zone.py
+++ Source/source_code/initialize_zone.py
@@ -6,16 +6,21 @@ import datetime
 # but since The India lies near equator and our size of each zone = 1Km
 # our assumption will introduce a marginal error that can be ignored
 
-# print delx,dely
+# the code written below intialize the database for the first time before the prodct can be used
+# it is meant to populate zone table , whos values wont change throught the usage of the product
+# zone_info that stores the initial value for the cost of advertisement for each zone and no of bundles in each zone
 x,y = left_extreme,bottom_extreme
 zone_no = 1
+# store the time of population of the database meant to be as min time
 cur  = datetime.datetime.now()
 while y < top_extreme :
     x = left_extreme
     while x < right_extreme :
         print x,y,zone_no
+        # making entery into zone table
         z = zone(bottom_left_coordinate_x=x,bottom_left_coordinate_y=y)
         z.save()
+        #making entery into zone_info table
         zone_info(zone_id=z.id,week=0,cost=100,no_of_bundles=10).save()
         running_slots(zone_id=z.id,slot=1,start_time=cur).save()
         zone_no += 1
